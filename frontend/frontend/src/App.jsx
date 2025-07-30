c6f723fc0ded34c4038648affc31e39ae092302a
Gowthamblue <gowtham104050@gmail.com>
2025-07-30 14:25:26 +0530

    First

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -8,7 +8,7 @@ import { Tooltip } from 'react-tooltip';
 import 'react-tooltip/dist/react-tooltip.css';
 import Confetti from 'react-confetti';
 
-const API = 'http://localhost:3000/api';
+const API = 'https://to-do-app-wj54.onrender.com/api';
 
 const priorityColors = {
   Low: '#4ade80',
